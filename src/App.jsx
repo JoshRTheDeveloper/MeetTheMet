40b5842e8956abb643b5e9be763b9808ba0aeab1
Josh Roth <raider4414@gmail.com>
2025-02-02 13:42:11 -0600

    added some styling and working on the api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './app.css'
+import axios from 'axios';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+const BASE_URL = ''
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+  
+     <div className='main-container'>
+ 
+ 
+<h1>The Metropolitian</h1>
+<h1>Mesuem of Art</h1>
+<div className='search-bar'>
+<input
+        type="text"
+        placeholder="Search..."
+        
+      />
+
+    </div>
+  
+     </div>
+    
     </>
   )
 }
