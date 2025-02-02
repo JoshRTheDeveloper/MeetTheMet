2190771913ffa8a49e8de5410d8480d3a70e619a
Josh Roth <raider4414@gmail.com>
2025-02-02 14:26:22 -0600

    API call for departments working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './app.css'
 import axios from 'axios';
 
 
 function App() {
-const BASE_URL = ''
+const BASE_URL = 'https://collectionapi.metmuseum.org/public/collection/v1/';
+const [departments, setDepartments] = useState([]);
+const apiService = axios.create({
+  baseURL: BASE_URL
+});
+
+const getDepartments = async () => {
+  try {
+    const result = await apiService.get('/departments');
+    setDepartments(result.data.departments);
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+  };
+
+
+useEffect(() => {
+  getDepartments();
+  
+}, []);
+console.log(departments)
 
   return (
     <>
@@ -17,6 +37,18 @@ const BASE_URL = ''
 <h1>The Metropolitian</h1>
 <h1>Mesuem of Art</h1>
 <div className='search-bar'>
+{departments.length > 0 ? (
+        <div>
+         
+          <ul>
+          {departments.map((department) => (
+            <li key={department.departmentId}>{department.displayName}</li> 
+          ))}
+        </ul>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
 <input
         type="text"
         placeholder="Search..."
