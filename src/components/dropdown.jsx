8b4b34ac557441594759a6635ffaa07fe5d5ea3e
Josh Roth <raider4414@gmail.com>
2025-02-02 18:41:14 -0600

    custom dropdown created

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './dropdown.css';
+
+function Dropdown ({options, onSelect}) {
+ const [isOpen, setIsOpen] = useState(false);
+ const [selectedDepartment, setSelectedDepartment] = useState(null);
+
+ const clickDropdown = () => setIsOpen(!isOpen);
+
+ const onClickSelect = (option) => {
+  setSelectedDepartment(option);
+  onSelect(option);
+  setIsOpen(false);
+ };
+
+return (
+ <div className='dropdown'>
+  <div className='dropdown-head' onClick={clickDropdown}>
+   {selectedDepartment.displayName}
+</div>
+{isOpen && (
+<div className='dropdown-selections'>
+ {options.map((option) => (
+  <div key= {option.departmentId} className='dropdown-selection' onClick={() => onClickSelect(option)}> 
+    {option.displayName}
+   </div>
+   
+ 
+ ))}
+
+</div>
+)};
+
+
+
+
+
+ </div>
+)
+
+}
